d941b5112bde035051995055e101f47481733c14
akarpisz <61389971+akarpisz@users.noreply.github.com>
2020-06-12 19:45:51 -0600

    server paths ('/','/notes', and '/api/notes') set and function. gitignore file added

--- server.js
+++ server.js
@@ -1,4 +1,32 @@
 const express = require("express");
-
+const path = require("path");
+const fs = require("fs");
 const app = express();
 
+const PORT = 5000;
+global.appRoot = path.resolve(__dirname);
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(appRoot + "/public/", "index.html"));
+});
+
+app.get("/notes", function(req,res) {
+    res.sendFile(path.join( appRoot + "/public/", "notes.html"));
+});
+
+app.get("/api/notes", function(req, res){
+    var notes = fs.readFileSync("./db/db.json", "utf-8", function(err, data){
+        if (err) throw err;
+        return data;
+    })
+    return res.json(JSON.parse(notes));
+});
+
+
+
+app.listen(PORT, function(){
+    console.log(`listening on Port: ${PORT}`);
+})
